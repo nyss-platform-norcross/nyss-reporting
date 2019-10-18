b2aa08b2f9db032d2cc7b4fd3b27d413dc47531e
milenkovorkapic <milenko@hackages.io>
2019-10-18 13:40:52 +0200

    Sms formatting

--- lib/sendSMS.dart
+++ lib/sendSMS.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_sms/flutter_sms.dart';
+import 'dart:async';
 
-void _sendSMS(String message, List<String> recipents) async {
+class SMSUtility {
+
+static void sendSMS(String message, List<String> recipents) async {
   String _result =
       await FlutterSms.sendSMS(message: message, recipients: recipents)
           .catchError((onError) {
     print(onError);
   });
   print(_result);
+}
+
 }
