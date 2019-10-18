685359d03b414c7524b62dadbc9a120d8ce5a422
milenkovorkapic <milenko@hackages.io>
2019-10-18 14:55:37 +0200

    Counter widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "selectHealthRisk.dart";
+import "peopleCounter.dart";
 import "sendSMS.dart";
 
 class VisibilityExample extends StatefulWidget {
@@ -18,6 +19,65 @@ class _VisibilityExampleState extends State {
   num _maleOverFive = 0;
   num _femaleUnderFive = 0;
   num _femaleOverFive = 0;
+
+  void addMaleUnderFive(){
+    setState(() {
+      _maleUnderFive = _maleUnderFive + 1;
+    });
+  }
+   void addMaleOverFive(){
+    setState(() {
+      _maleOverFive = _maleOverFive + 1;
+    });
+  }
+
+ void addFemaleUnderFive(){
+    setState(() {
+      _femaleUnderFive = _femaleUnderFive + 1;
+    });
+  }
+
+ void addFemaleOverFive(){
+    setState(() {
+      _femaleOverFive = _femaleOverFive + 1;
+    });
+  }
+
+
+  void decrementMaleUnderFive(){
+    if(_maleUnderFive == 0 ){
+      return;
+    }
+    setState(() {
+      _maleUnderFive = _maleUnderFive - 1;
+    });
+  }
+   void decrementMaleOverFive(){
+     if(_maleOverFive == 0 ){
+      return;
+    }
+    setState(() {
+      _maleOverFive = _maleOverFive - 1;
+    });
+  }
+
+ void decrementFemaleUnderFive(){
+   if(_femaleUnderFive == 0 ){
+      return;
+    }
+    setState(() {
+      _femaleUnderFive = _femaleUnderFive - 1;
+    });
+  }
+
+ void decrementFemaleOverFive(){
+   if(_femaleOverFive == 0 ){
+      return;
+    }
+    setState(() {
+      _femaleOverFive = _femaleOverFive - 1;
+    });
+  }
 
   // TODO: Get that data from the backend
   List<String> phoneNumbers = ["+32000000000"];
@@ -63,13 +123,20 @@ class _VisibilityExampleState extends State {
                         selectHealthRisk: _selectHealthRisk)),
                 Visibility(
                   visible: _isVisible,
-                  child: Card(
-                    child: new ListTile(
-                      title: Center(
-                        child: new Text('B'),
-                      ),
-                    ),
-                  ),
+                  child: PeopleCounter(
+                    maleOverFive: _maleOverFive,
+                    maleUnderFive: _maleUnderFive,
+                    femaleOverFive: _femaleOverFive,
+                    femaleUnderFive: _femaleUnderFive,
+                    addMaleUnderFive: addMaleUnderFive,
+                    addMaleOverFive: addMaleOverFive,
+                    addFemaleUnderFive: addFemaleUnderFive,
+                    addFemaleOverFive: addFemaleOverFive,
+                    decrementMaleUnderFive: decrementMaleUnderFive,
+                    decrementMaleOverFive: decrementMaleOverFive,
+                    decrementFemaleUnderFive: decrementFemaleUnderFive,
+                    decrementFemaleOverFive: decrementFemaleOverFive
+                  )
                 ),
                 RaisedButton(
                   child: Text('Submit my data'),
